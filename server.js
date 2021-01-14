92110b96775575cc5fd342b6cd266033b3c5712b
terranadams <terran.adams7225@stu.mtec.edu>
2021-01-14 10:45:36 -0700

    worked on server messages

--- server.js
+++ server.js
@@ -9,28 +9,32 @@ let server = net.createServer(client => {
     client.write('Client ' + (clients.length) + ' has entered the chat, welcome!')
 
     client.on('data', data => {
+
         broadcast(data, client)
+
     })
 
     client.on('end', client.end)
-
-    process.stdin.pipe(client)
-
+    // all pipe does is forward events to other streams  //
+    process.stdin.on('data', data => {
+        client.write(`Server: ${data}`)
+    })
+    
     function broadcast(message, sender) {
         let theIndex
         clients.forEach((client, index) => {
-            if (client === sender) {theIndex = index + 1; return}
+            if (client === sender) { theIndex = index + 1; return }
         })
-        clients.forEach(function (client, index) {
-          // Don't want to send it to sender
-          if (client === sender) return;
-          client.write('Client ' + (theIndex) + ' says: ' + message)
+        clients.forEach(function (client) {
+            // Don't want to send it to sender
+            if (client === sender) return;
+            client.write('Client ' + (theIndex) + ': ' + message)
 
         });
-        let serverOutput = `Client ${theIndex}  says: ${message}`
+        let serverOutput = `Client ${theIndex}: ${message}`
         // Log it to the server output too
         process.stdout.write(serverOutput)
-      }
+    }
 
 })
 
