bdd742a2d10df95c43e8b43a3568bc2e2e0e2514
terranadams <terran.adams7225@stu.mtec.edu>
2021-01-14 19:24:13 -0700

    gave each user an id and got ids to display correctly

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const net = require('net')
 let clients = []
 let server = net.createServer(client => {
     client.name = client.remoteAddress + client.remoteAddress
-
+    client.id = clients.length + 1
     clients.push(client)
+    console.log('Number of clients: ' + clients.length)
 
-    client.write('Client ' + (clients.length) + ' has entered the chat, welcome!')
+    client.write('Welcome to the chatroom!')
 
     client.on('data', data => {
 
@@ -14,24 +15,29 @@ let server = net.createServer(client => {
 
     })
 
-    client.on('end', client.end)
+    client.on('end', () => {
+        clients.splice(clients.indexOf(socket), 1);
+        broadcast(client.name + " left the chat.\n");
+    })
+
+
     // all pipe does is forward events to other streams  //
     process.stdin.on('data', data => {
         client.write(`Server: ${data}`)
     })
-    
+
     function broadcast(message, sender) {
         let theIndex
         clients.forEach((client, index) => {
-            if (client === sender) { theIndex = index + 1; return }
+            if (client === sender) { theIndex = index; return }
         })
         clients.forEach(function (client) {
             // Don't want to send it to sender
             if (client === sender) return;
-            client.write('Client ' + (theIndex) + ': ' + message)
+            client.write('Client ' + (clients[theIndex].id) + ': ' + message)
 
         });
-        let serverOutput = `Client ${theIndex}: ${message}`
+        let serverOutput = `Client ${clients[theIndex].id}: ${message}`
         // Log it to the server output too
         process.stdout.write(serverOutput)
     }
