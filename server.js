b76437d0d25d31912c1c687a81616f653e63af65
terranadams <terran.adams7225@stu.mtec.edu>
2021-01-22 07:40:53 -0700

    added username functionality

--- server.js
+++ server.js
@@ -6,78 +6,57 @@ let clients = []
 let newClientId = 1
 let server = net.createServer(client => {
     client.name = client.remoteAddress + client.remoteAddress // This refers to each clients address
-    client.id = newClientId // We use this to prevent new clients from having the name of old clients
+    client.id = `Client-${newClientId}` // We use this to prevent new clients from having the name of old clients
     newClientId++ // We use this to prevent new clients from having the name of old clients
     clients.push(client) // We increment the number so that regardless of how many clients are still in the chat, they'll never have the same name
-    chatLog += `Client ${client.id} has joined the chat. \n`
+    chatLog += `${client.id} has joined the chat. \n`
     fs.writeFile('./chat.log', chatLog, () => {})
     console.log('\n' + 'A new client has arrived.')
     console.log(`Number of clients: ${clients.length}` + '\n')
 
-    informClientAddition(client) // This is a function I made that informs the other clients of a new client that joined the chat
-
-    client.write('\n' + 'You are connected as Client ' + client.id + '\n' + 'Welcome to the chatroom!' + '\n') 
+    broadcast(`${client.id} has joined the chat! \n`, client)
+    
+    client.write('\n' + 'You are connected as ' + client.id + '\n' + 'Welcome to the chatroom!' + '\n') 
  
     client.on('data', data => {
-        broadcast(data, client) // This is a custom function that rebroadcasts client messages to the other clients without resending it back to the sender
-        chatLog += `Client ${client.id}: ${data}`
-        fs.writeFile('./chat.log', chatLog, () => {})
+        let dataString = data.toString().replace(/\r?\n|\r/, '')
+        let dataArray = dataString.split(' ')
+        if (dataArray[0] == '/username' && dataArray[1]) {
+            if (dataArray[2]) client.write(`You passed in too many arguments, try again.`)
+            else if (client.id == dataArray[1]) client.write(`You're already using that username.`)
+            else if (clients.filter(x => x.id == dataArray[1]).length > 0) client.write(`Someone else is using that username.`)
+            else {
+                client.write(`\nYour new username is ${dataArray[1]}. \n`)
+                broadcast(`${client.id}'s new username is ${dataArray[1]}. \n`, client)
+                chatLog += `${client.id}'s new username is ${dataArray[1]}. \n`
+                client.id = dataArray[1]
+                fs.writeFile('./chat.log', chatLog, () => {})
+            }
+        } else {
+            broadcast(`\n${client.id}: ${dataString}\n`, client)
+            chatLog += `${client.id}: ${dataString}\n`
+            fs.writeFile('./chat.log', chatLog, () => {})
+        }
     })
 
     client.on('end', () => {
-        informClientRemoval(client) // This is a custom function that informs the other clients of this client's disconnection
+        broadcast(`${client.id} has left the chat. \n`, client) // This is a custom function that informs the other clients of this client's disconnection
         clients.splice(clients.indexOf(client), 1); // This removes this specific client's spot in the array of clients
         console.log(`Number of clients: ${clients.length}` + '\n')
-        chatLog += `Client ${client.id} has left the chat. \n`
+        chatLog += `${client.id} has left the chat. \n`
         fs.writeFile('./chat.log', chatLog, () => {})
     })
 
     
 
     function broadcast(message, sender) { // We prevent resending the client's message being sent back to itself by capturing its index
-        sender.write('')
-        let theIndex
-        clients.forEach((client, index) => {
-            if (client === sender) { theIndex = index; return }
-        })
         clients.forEach(x => {
             // Don't want to send it to sender
             if (x === sender) return;
-            x.write('\n' + 'Client ' + (clients[theIndex].id) + ': ' + message)
+            x.write(message)
         })
-        let serverOutput = '\n' + `Client ${clients[theIndex].id}: ${message}` + '\n'
-        // Log it to the server output too
-        process.stdout.write(serverOutput)
+        console.log(message)
     }
-
-    function informClientRemoval(sender) { // We loop through the array of clients and message them about which client left the chat
-        let theIndex
-        clients.forEach((client, index) => {
-            if (client === sender) { theIndex = index; return }
-        })
-        clients.forEach(x => {
-            // Don't want to send it to sender
-            if (x === sender) return;
-            x.write(`Client ${clients[theIndex].id} has left the chat.` + '\n')
-        })
-        let serverOutput = `Client ${clients[theIndex].id} has left the chat.`
-        // Log it to the server output too
-        process.stdout.write(serverOutput)
-    }
-
-    function informClientAddition(sender) { // We prevent resending the message about a new client joining by capturing its index
-        let theIndex
-        clients.forEach((client, index) => {
-            if (client === sender) { theIndex = index; return }
-        })
-        clients.forEach(x => {
-            // Don't want to send it to sender
-            if (x === sender) return;
-            x.write(`Client ${clients[theIndex].id} has joined the chat.` + '\n')
-        })
-        process.stdout.write('')
-    }
-
 })
 
 // We capture the server message outside of the main client function
