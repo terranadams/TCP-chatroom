5c8999fdf9f01683cf973fc5e7c1a2115a51e329
terranadams <terran.adams7225@stu.mtec.edu>
2021-01-26 10:13:04 -0700

    changed writeFile to appendFile, all done.

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const net = require('net')
 const fs = require('fs')
 
 let password = 'skatelife'
-let chatLog = ''
+// let chatLog = ''
 let clients = []
 let newClientId = 1
+fs.writeFile('./chat.log', '', () => {})
 let server = net.createServer(client => {
     client.name = client.remoteAddress + client.remoteAddress // This refers to each clients address
     client.id = `Client-${newClientId}` // We use this to prevent new clients from having the name of old clients
     newClientId++ // We use this to prevent new clients from having the name of old clients
     clients.push(client) // We increment the number so that regardless of how many clients are still in the chat, they'll never have the same name
-    chatLog += `${client.id} has joined the chat. \n`
-    fs.writeFile('./chat.log', chatLog, () => {})
+    // chatLog += `${client.id} has joined the chat. \n`
+    fs.appendFile('./chat.log', `${client.id} has joined the chat. \n`, () => {})
     console.log('\n' + 'A new client has arrived.')
     console.log(`Number of clients: ${clients.length}` + '\n')
 
@@ -22,16 +23,17 @@ let server = net.createServer(client => {
     client.on('data', data => {
         let dataString = data.toString().replace(/\r?\n|\r/, '')
         let dataArray = dataString.split(' ')
-        if (dataArray[0] == '/username' && dataArray[1]) {
+        if (dataArray[0] == '/username' && !dataArray[1]) client.write('Not enough arguments. Try again.')
+        else if (dataArray[0] == '/username' && dataArray[1]) {
             if (dataArray[2]) client.write(`You passed in too many arguments, try again.`)
             else if (client.id == dataArray[1]) client.write(`You're already using that username.`)
             else if (clients.filter(x => x.id == dataArray[1]).length > 0) client.write(`Someone else is using that username.`)
             else {
                 client.write(`\nYour new username is ${dataArray[1]}. \n`)
                 broadcast(`${client.id}'s new username is ${dataArray[1]}. \n`, client)
-                chatLog += `${client.id}'s new username is ${dataArray[1]}. \n`
+                // chatLog += `${client.id}'s new username is ${dataArray[1]}. \n`
+                fs.appendFile('./chat.log', `${client.id}'s new username is ${dataArray[1]}. \n`, () => {})
                 client.id = dataArray[1]
-                fs.writeFile('./chat.log', chatLog, () => {})
             }
         } else if (dataArray[0] == '/clientlist') {
             if (dataArray[1]) {
@@ -39,6 +41,7 @@ let server = net.createServer(client => {
             } else {
                 clientList = clients.map(x => x.id).join(' ')
                 client.write(clientList)
+                fs.appendFile('chat.log', `${client.id} checked the clientlist.\n`, () => {})
             }
         } else if (dataArray[0] == '/kick') {
             if (!dataArray[2]) {
@@ -70,15 +73,15 @@ let server = net.createServer(client => {
                 } else {
                     message = dataArray.slice(2, dataArray.length).join(' ')
                     reciever.write(`Whisper from ${client.id}: ${message}\n`)
-                    chatLog += `Whisper from ${client.id} to ${reciever.id}: ${message}\n`
-                    fs.writeFile('./chat.log', chatLog, () => {})
+                    // chatLog += `Whisper from ${client.id} to ${reciever.id}: ${message}\n`
+                    fs.appendFile('./chat.log', `Whisper from ${client.id} to ${reciever.id}: ${message}\n`, () => {})
                     console.log(`Whisper from ${client.id} to ${reciever.id}: ${message}\n`)
                 }
             }
         } else {
             broadcast(`\n${client.id}: ${dataString}\n`, client)
-            chatLog += `${client.id}: ${dataString}\n`
-            fs.writeFile('./chat.log', chatLog, () => {})
+            // chatLog += `${client.id}: ${dataString}\n`
+            fs.appendFile('./chat.log', `${client.id}: ${dataString}\n`, () => {})
         }
         
     })
@@ -87,8 +90,8 @@ let server = net.createServer(client => {
         broadcast(`${client.id} has left the chat. \n`, client) // This is a custom function that informs the other clients of this client's disconnection
         clients.splice(clients.indexOf(client), 1); // This removes this specific client's spot in the array of clients
         console.log(`Number of clients: ${clients.length}` + '\n')
-        chatLog += `${client.id} has left the chat. \n`
-        fs.writeFile('./chat.log', chatLog, () => {})
+        // chatLog += `${client.id} has left the chat. \n`
+        fs.appendFile('./chat.log', `${client.id} has left the chat. \n`, () => {})
     })
 
     function broadcast(message, sender) { // We prevent resending the client's message being sent back to itself by capturing its index
@@ -105,8 +108,8 @@ let server = net.createServer(client => {
             if (x.id == kicked.id) return
             x.write(`\nServer: ${kicked.id} has been kicked out of the chat. \n`)
         })
-        chatLog += `Server: ${kicked.id} has been kicked out of the chat. \n`
-        fs.writeFile('./chat.log', chatLog, () => {})
+        // chatLog += `Server: ${kicked.id} has been kicked out of the chat. \n`
+        fs.appendFile('./chat.log', `Server: ${kicked.id} has been kicked out of the chat. \n`, () => {})
         console.log(`${kicked.id} has been kicked out of the chat. \n`)
     }
 })
@@ -116,8 +119,8 @@ process.stdin.on('data', data => { // This must be outside of the createServer c
     clients.forEach(x => {
         x.write(`Server: ${data}`)
     })
-    chatLog += `Server: ${data}`
-    fs.writeFile('./chat.log', chatLog, () => {})
+    // chatLog += `Server: ${data}`
+    fs.appendFile('./chat.log', `Server: ${data}`, () => {})
 })
 
 server.listen(5000, () => {
